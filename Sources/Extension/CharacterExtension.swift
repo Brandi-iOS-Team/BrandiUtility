b18f8440a2b2b37e88a75e9b3619cc172e344391
Joo Hee Kim <imjhk03@gmail.com>
2021-02-10 09:26:30 +0900

    Add Emoji related extensions

---
+++ Sources/Extension/CharacterExtension.swift
@@ -0,0 +1,19 @@
+//
+//  CharacterExtension.swift
+//  Brandi
+//
+//  Created by Joo Hee Kim on 21. 02. 05..
+//
+
+import Foundation
+
+public extension Character {
+    var isSimpleEmoji: Bool {
+        guard let firstScalar = unicodeScalars.first else { return false }
+        return firstScalar.properties.isEmoji && firstScalar.value > 0x238C
+    }
+
+    var isCombinedIntoEmoji: Bool { unicodeScalars.count > 1 && unicodeScalars.first?.properties.isEmoji ?? false }
+
+    var isEmoji: Bool { isSimpleEmoji || isCombinedIntoEmoji }
+}

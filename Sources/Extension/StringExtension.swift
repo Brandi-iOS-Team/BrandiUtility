2b67af0db2b55dd263ddb2c78eec9d113499f432
Joo Hee Kim <imjhk03@gmail.com>
2021-02-18 16:39:41 +0900

    Remove duplicated function

--- Sources/Extension/StringExtension.swift
+++ Sources/Extension/StringExtension.swift
@@ -32,25 +32,6 @@ public extension String {
         return nil
     }
     
-    var containsEmoji: Bool { //string에 이모지가 포함된 경우 return true
-        for scalar in unicodeScalars {
-            switch scalar.value {
-            case 0x1F600...0x1F64F, // Emoticons
-                 0x1F300...0x1F5FF, // Misc Symbols and Pictographs
-                 0x1F680...0x1F6FF, // Transport and Map
-                 0x2600...0x26FF,   // Misc symbols
-                 0x2700...0x27BF,   // Dingbats
-                 0xFE00...0xFE0F,   // Variation Selectors
-                 0x1F900...0x1F9FF, // Supplemental Symbols and Pictographs
-                 0x1F1E6...0x1F1FF: // Flags
-                return true
-            default:
-                continue
-            }
-        }
-        return false
-    }
-    
     func convertHtml() -> NSMutableAttributedString {
         guard let data = data(using: .utf8) else { return NSMutableAttributedString() }
         do {

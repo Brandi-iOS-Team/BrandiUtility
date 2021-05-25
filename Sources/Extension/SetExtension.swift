056531a519e384e7809444dae84c74c655f18e4f
NohEunTae <dmsxoeldk@naver.com>
2021-05-25 17:55:51 +0900

    etnoh - set > insert 코드 간소화

--- Sources/Extension/SetExtension.swift
+++ Sources/Extension/SetExtension.swift
@@ -10,11 +10,7 @@ import Foundation
 public extension Set {
     
     @discardableResult mutating func insert(_ newMembers: [Set.Element]) -> [(inserted: Bool, memberAfterInsert: Set.Element)] {
-        var returnArray: [(inserted: Bool, memberAfterInsert: Set.Element)] = []
-        newMembers.forEach { (member) in
-            returnArray.append(self.insert(member))
-        }
-        return returnArray
+        newMembers.map { insert($0) }
     }
     
 }
